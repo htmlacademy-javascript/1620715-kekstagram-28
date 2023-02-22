b1a210d144ff9081db8ede14aaf301e41bef5f94
Евгений Снудин <ev.snudin@yandex.ru>
2023-02-22 23:32:56 +0300

    Исправляет функции

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,20 @@
 const checkString = (string, length) => string.length <= length;
 
-const checkPalindrom = (string) => {
-  const tempString = string.toLowerCase() .replaceAll(' ', '');
-  let reverseString = '';
-  for (let i = tempString.length - 1; i >= 0; i--) {
-    reverseString += tempString[i];
-  }
-  return tempString === reverseString;
-}
+const checkPalindrome = (string) => {
+  string = string.toLowerCase().replaceAll(' ', '');
+  return string === string.split('').reverse().join('') ;
+};
 
 const extractNumber = (string) => {
-  let result = '';
-  for (let i = 0; i < string.length; i++) {
-    const stringletter = string[i];
-    const parsedLetter = parseInt(stringletter, 10);
-    const isNaN = Number.isNaN(parsedLetter);
-    if(!isNaN)
-      { result += string[i];
-        }
-  }
-    return parseInt(result, 10);
-}
+  string = string.replace(/\D/g, '');
+  return parseInt(string, 10);
+};
 
-const myPadStart = (string, minLeght, pad) => {
-  const actualPad = minLeght - string.length;
-if(actualPad <= 0) {
+const createNewString = (string, minLenght, pad) => {
+  while (string.length < minLenght) {
+    string = pad.slice (0, minLenght - string.length) + string;
+  }
   return string;
-}
-  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
-}
+};
 
 
